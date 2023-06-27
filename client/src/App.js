9bbbc8668f36b37c3b46d85ea057eff7ed8e5067
Rounak Shrivastav <shrivastavrounak6@gmail.com>
2023-06-27 12:52:56 +0530

    solve folder not opening

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import { uploadFile } from './services/api';
+
+function App() {
+  const [file, setFile] = useState('');
+  const [result, setResult] = useState('');
+
+  const fileInputRef = useRef();
+
+  const url = 'https://wallpapercave.com/wp/wp6411468.jpg';
+
+  useEffect(() => {
+    const getImage = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        const response = await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  }, [file])
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+  return (
+    <div className='container'>
+     
+      <img src={url} className='img' alt='img' />
+      <div className='wrapper'>
+        
+        <h1>File Sharing!</h1>
+        <p>Upload and share the download link.</p>
+        
+        <button onClick={() => onUploadClick()}>Upload</button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+
+        <a href={result} target='_blank' rel="noreferrer">{result}</a> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
